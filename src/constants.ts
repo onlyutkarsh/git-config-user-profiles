ecfbcba1d7e531b59c42be08014689c981f52949
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2019-12-11 15:20:03 +0000

    rename extension

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const Commands = {
-    CREATE_USER_PROFILE: "git-config-user.createUserProfile",
-    GET_USER_PROFILE: "git-config-user.selectUserProfile",
-    EDIT_USER_PROFILE: "git-config-user.editUserProfile",
+    CREATE_USER_PROFILE: "git-config-user-profiles.createUserProfile",
+    GET_USER_PROFILE: "git-config-user-profiles.selectUserProfile",
+    EDIT_USER_PROFILE: "git-config-user-profiles.editUserProfile",
 };
