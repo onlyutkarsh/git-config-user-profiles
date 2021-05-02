6f23ea26f3f21d0eb3d22d6706ff0d7c7a644ea3
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2021-05-02 21:27:52 +0100

    mocha test explorer, chai for assert

--- test/suite/main.test.ts
+++ test/suite/main.test.ts
@@ -1,12 +1,15 @@
-import * as assert from "assert";
+import { assert } from "chai";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
 
 suite("Main", () => {
-  test("Should start extension @assistant", async () => {
-    const started = vscode.extensions.getExtension("onlyutkarsh.github-config-user-profiles");
-    assert.notStrictEqual(started, undefined);
-    assert.strictEqual(started?.isActive, true);
+  test("should extension be loaded on startup", async () => {
+    const extension = vscode.extensions.getExtension("onlyutkarsh.git-config-user-profiles");
+    if (!extension?.isActive) {
+      await extension?.activate();
+    }
+    assert.isOk(extension);
+    assert.isTrue(extension?.isActive);
   });
 });
