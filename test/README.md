aac1ef608f5580566bc3fffaf4fa8f6c650af291
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2021-08-29 22:31:08 +0100

    test setup using mocha

---
+++ test/README.md
@@ -0,0 +1,8 @@
+# Testing
+
+The tests under this folder are integration tests.
+
+## Running tests
+
+- In the terminal, run `npm run test-watch`
+- In VSCode select `Run and Debug` pane ( <kbd>Cmd + Shift + D </kbd>) and select `Run Extension Tests` and Run
--- test/suite/main.test.ts
+++ test/suite/main.test.ts
@@ -1,5 +1,17 @@
-describe("index", () => {
-  it("test", () => {
-    expect(1).toBe(1);
+import * as assert from "assert";
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from "vscode";
+
+// import * as myExtension from '../../extension';
+
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
+
+  test("Sample 1", () => {
+    assert.strictEqual([1, 2, 3].indexOf(1), 0);
+  });
+  test("Sample 2", () => {
+    assert.strictEqual([1, 2, 3].indexOf(5), -1);
   });
 });
